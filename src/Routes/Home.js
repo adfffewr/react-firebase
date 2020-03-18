d71b871d4fe8e4d672bea994b36a67652118064e
이성재 <adfffewr@nvaer.com>
2020-03-18 22:46:03 +0900

    코드 스플리팅 적용

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+
+const HomeLayout = lazy(() => import('../Layout/HomeLayout'));
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeLayout></HomeLayout>
+      <div>Home</div>
+    </Suspense>
+  );
 };
 
 export default Home;
