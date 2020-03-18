515f6eb60cca0bf0aeac93620b4a58689baf66b9
이성재 <adfffewr@nvaer.com>
2020-03-18 22:23:43 +0900

    header component 작업

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -2,6 +2,22 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 const GlobalStyles = createGlobalStyle`
   ${reset};  
+  @font-face { font-family: 'NanumBarunGothic'; font-style: normal; font-weight: 400; src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot'); src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.eot?#iefix') format('embedded-opentype'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.woff') format('woff'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWeb.ttf') format('truetype'); } 
+  @font-face { font-family: 'NanumBarunGothic'; font-style: normal; font-weight: 700; src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.eot'); src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.eot?#iefix') format('embedded-opentype'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.woff') format('woff'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebBold.ttf') format('truetype') } 
+  @font-face { font-family: 'NanumBarunGothic'; font-style: normal; font-weight: 300; src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebLight.eot'); src: url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebLight.eot?#iefix') format('embedded-opentype'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebLight.woff') format('woff'), url('//cdn.jsdelivr.net/font-nanumlight/1.0/NanumBarunGothicWebLight.ttf') format('truetype'); } 
+  
+  body {
+    background-color: rgb(31, 41, 51);
+    color: rgb(245, 247, 250);
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 1.65;
+    font-family: 'NanumBarunGothic', sans-serif;
+  }
+  
+  @media screen and (min-width:768px) {
+    font-size: 18px;
+  }
 `;
 
 export default GlobalStyles;
--- src/Components/common/header.js
+++ src/Components/common/header.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderBox = styled.header`
+  padding: 20px;
+  a {
+    text-decoration: none;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 2em 4em 4em;
+  }
+`;
+const NavBox = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Logo = styled(Link)`
+  color: rgb(245, 247, 250);
+`;
+const LinkTag = styled(Link)`
+  color: #00bfa6;
+  transition: color 0.2s;
+  &:hover {
+    color: rgb(245, 247, 250);
+  }
+`;
+const NavListBox = styled.ul`
+  display: flex;
+  align-items: center;
+  li {
+    margin-left: 15px;
+  }
+`;
 
 const Header = () => {
   return (
     <>
-      <header>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">about</Link>
-          </li>
-        </ul>
-      </header>
+      <HeaderBox>
+        <NavBox>
+          <h1>
+            <Logo to="/">CodingPalette</Logo>
+          </h1>
+          <NavListBox>
+            <li>
+              <LinkTag to="/">Home</LinkTag>
+            </li>
+            <li>
+              <LinkTag to="/about">about</LinkTag>
+            </li>
+          </NavListBox>
+        </NavBox>
+      </HeaderBox>
     </>
   );
 };
